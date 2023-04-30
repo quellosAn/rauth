8f169a0837132899bfb3748d149f2f5148b98de5
Andrew Quellos <quellosa@att.net>
2023-04-30 15:38:32 -0400

    Start of implementation of user session code cull

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::collections::VecDeque;
 use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+use std::time::SystemTime;
+use std::collections::HashMap;
 use dashmap::DashMap;
 use http_body_util::Full;
 use http_body_util::Empty;
@@ -12,14 +15,17 @@ use hyper::body::Bytes;
 use hyper::Method;
 use hyper::StatusCode;
 use tokio::net::TcpListener;
-use std::collections::HashMap;
+use tokio::task::JoinHandle;
+use tokio::time::{ interval, Duration, Instant };
 use uuid::Uuid;
-use std::time::{SystemTime};
+
+
 
 #[tokio::main]
 async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 9000));
     let listener = TcpListener::bind(addr).await.unwrap();
+    let identityStore = IdentityStore::new();
     loop {
         let (stream, _) = listener.accept().await.unwrap();
         tokio::spawn(async move {
@@ -54,7 +60,7 @@ async fn auth_process(
             *bad_request.status_mut() = StatusCode::BAD_REQUEST;
             Ok(bad_request)     
             
-        },
+        }
         _ => {
             let mut not_found = Response::new(empty());
             *not_found.status_mut() = StatusCode::NOT_FOUND;
@@ -77,17 +83,47 @@ fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
 }
 
 struct IdentityStore {
-    session_queue: VecDeque<Session>,
+    session_queue: Arc<Mutex<VecDeque<Session>>>,
     session_store: DashMap<String, SessionRecord>,
-    
+    cleanup_handle: JoinHandle<()>
+}
+
+impl IdentityStore {
+    fn new() -> IdentityStore {
+        let local_session_queue = Arc::new(Mutex::new(VecDeque::new()));
+        let local_session_store = DashMap::new();
+        IdentityStore {
+            session_queue: local_session_queue.clone(),
+            session_store: local_session_store,
+            cleanup_handle: tokio::spawn(async move {
+                let mut loop_interval = interval(Duration::from_secs(60));
+                loop {
+                    loop_interval.tick().await;
+                    //TODO: write some sort of logic to resize queue if required
+                    let mut locked_queue = local_session_queue.lock().unwrap();
+                    if !locked_queue.is_empty() {
+                        let current_timestamp = Instant::now();
+                        let removal_point = locked_queue.partition_point(
+                            |session| current_timestamp.duration_since(session.timestamp).as_secs() > 60
+                        );
+                        if removal_point != 0 {
+                            locked_queue.drain(removal_point + 1..);
+                        }
+                    }
+                    
+                }
+            })
+        } 
+    }
 }
 
 struct Session {
     session_id: Uuid,
-    timestamp: SystemTime
+    timestamp: Instant
 }
 
 struct SessionRecord {
     session_id: Uuid,
-    auth_code: Uuid
+    auth_code: Uuid,
+    timestamp: Instant
 }
